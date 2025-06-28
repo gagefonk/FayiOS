74cf96341c6a412b2953ac07a67f92795c28f539
Gage <gagefonk@gmail.com>
2025-06-28 11:40:46 -0700

    Initial Commit

---
+++ FayiOS/FayiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FayiOSApp.swift
+//  FayiOS
+//
+//  Created by Gage Fonk on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FayiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
